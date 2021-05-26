f1b28dbac0782594ed1e7f639ec8184caeab4b98
JohnYehyo <836524969@qq.com>
2021-05-26 14:48:14 +0800

    feat: 启动后记录进程号

--- src/main/java/com/johnyehyo/servermanage/ServerManageApplication.java
+++ src/main/java/com/johnyehyo/servermanage/ServerManageApplication.java
@@ -2,12 +2,16 @@ package com.johnyehyo.servermanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ServerManageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerManageApplication.class, args);
+//        SpringApplication.run(ServerManageApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(ServerManageApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 }
