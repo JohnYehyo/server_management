00ff723b54ed04f98334b1f97ff6b19db0bf3da7
JohnYehyo <836524969@qq.com>
2021-05-28 16:49:43 +0800

    feat: 启动后记录进程号

--- src/main/java/com/johnyehyo/servermanage/ServerManageApplication.java
+++ src/main/java/com/johnyehyo/servermanage/ServerManageApplication.java
@@ -10,7 +10,7 @@ public class ServerManageApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(ServerManageApplication.class, args);
         SpringApplication springApplication = new SpringApplication(ServerManageApplication.class);
-        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.addListeners(new ApplicationPidFileWriter("./server_manage.pid"));
         springApplication.run(args);
     }
 
